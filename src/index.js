350afcc314f6ddebbaf6a5e8c8ae9ca2309d6602
arthurchic20 <arthurchic20@gmail.com>
2023-01-22 01:26:54 +0600

    init: start and end project

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    let arrOfNumber = expr.match(/(.{10}|.)/g)
+
+    for (let x = 0; x < arrOfNumber.length; x++) {
+
+        arrOfNumber[x] = arrOfNumber[x].match(/(.{2}|.)/g)
+        let subarray = arrOfNumber[x]
+        
+        for (let i = 0; i < subarray.length; i++) {
+            if (subarray[i] === '00') {
+                i -= 1;
+                subarray.shift()
+            } else if (subarray[i] === '10') {
+                subarray[i] = '.'
+            } else if (subarray[i] === '11') {
+                subarray[i] = '-'
+            }
+        }
+        
+        arrOfNumber[x] = arrOfNumber[x].join('')
+    }  
+
+    arrOfNumber.forEach(element => {
+        for (key in MORSE_TABLE) {
+            if (element === key ) {
+                res += MORSE_TABLE[key]
+            } else if (element == '**********') {
+                element = ' ' 
+                res += element
+            }
+        } 
+    });
+
+    return res
 }
 
 module.exports = {
